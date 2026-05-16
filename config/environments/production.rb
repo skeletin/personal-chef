0a30151e71880112d8f7a07fa313b6b492654229
Julian Smith <juliansmith94@hotmail.com>
2026-05-15 20:12:32 -0500

    update production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,4 +17,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.active_record.attributes_for_inspect = [ :id ]
   config.hosts << "chefboy.up.railway.app"
+  config.require_master_key = false
 end
